f4f6a279d0cd697dd4c8d807ab972cdb6483af29
Lucas Kaoru <lucasnohara51@gmail.com>
2026-09-20 01:34:34 -0300

    feat: adicionando rota de emprestar livro

--- src/routers/livroRoutes.js
+++ src/routers/livroRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { buscarLivrosController, buscarLivroPorIdController, criarLivroController, atualizarLivroController, deletarLivroController } = require("../controllers/livroController");
+const { buscarLivrosController, buscarLivroPorIdController, criarLivroController, atualizarLivroController, deletarLivroController, emprestarLivroController } = require("../controllers/livroController");
 
 router.get("/livros", buscarLivrosController);
 router.get("/livros/:id", buscarLivroPorIdController);
 router.post("/livros", criarLivroController);
 router.put("/livros/:id", atualizarLivroController);
 router.delete("/livros/:id", deletarLivroController);
+router.put("/livros/:id", emprestarLivroController);
 
 module.exports = router;
