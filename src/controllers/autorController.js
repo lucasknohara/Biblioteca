dc613ca23352b74291f47bcf6395f3a66a61ae7e
Lucas Kaoru <lucasnohara51@gmail.com>
2026-09-22 19:54:36 -0300

    fix: tirando try / cacth do autorController e livroController

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -1,45 +1,23 @@
-const autorModel = require("../models/autorModel.js");
 const autorService = require("../services/autorService.js");
 
 async function buscarAutoresController(req, res) {
-    try {
-        const resultado = await autorService.buscarAutoresService();
+    const resultado = await autorService.buscarAutoresService();
 
-        res.status(200).json(resultado);
-    } catch (error) {
-        console.log(error.message);
-        res.status(error.status ?? 500).json({
-            error: error.message
-        });
-    }
+    res.status(200).json(resultado);
 };
 
 async function criarAutorController(req, res) {
-    try {
-        const { nome } = req.body;
-        const resultado = await autorService.criarAutorService(nome);
+    const { nome } = req.body;
+    const resultado = await autorService.criarAutorService(nome);
 
-        res.status(201).send(resultado);
-    } catch (error) {
-        console.log(error.message);
-        res.status(error.status ?? 500).json({
-            error: error.message
-        });
-    }
+    res.status(201).send(resultado);
 };
 
 async function buscarAutorPorIdController(req, res) {
-    try {
-        const id = req.params.id;
-        const resultado = await autorService.buscaAutorPorIdService(id);
+    const id = req.params.id;
+    const resultado = await autorService.buscaAutorPorIdService(id);
 
-        res.status(200).json(resultado);
-    } catch (error) {
-        console.log(error.message);
-        res.status(error.status ?? 500).json({
-            error: error.message
-        });
-    }
+    res.status(200).json(resultado);
 };
 
 module.exports = {
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,87 +1,45 @@
 const livroService = require("../services/livroService.js");
 
 async function buscarLivrosController(req, res) {
-    try {
-        const resultado = await livroService.buscaLivrosService();
+    const resultado = await livroService.buscaLivrosService();
 
-        res.status(200).json(resultado);
-    } catch (error) {
-        console.log(error.message);
-        res.status(error.status ?? 500).json({
-            error: error.message
-        });
-    }
+    res.status(200).json(resultado);
 };
 
 async function buscarLivroPorIdController(req, res) {
-    try {
-        const id = req.params.id;
-        const resultado = await livroService.buscaLivrosPorIdService(id);
+    const id = req.params.id;
+    const resultado = await livroService.buscaLivrosPorIdService(id);
 
-        res.status(200).json(resultado);
-    } catch (error) {
-        console.log(error.message);
-        res.status(error.status ?? 500).json({
-            error: error.message
-        });
-    }
+    res.status(200).json(resultado);
 };
 
 async function criarLivroController(req, res) {
-    try {
-        const { titulo, autor_id, ano } = req.body;
-        const resultado = await livroService.criarlivroService(titulo, autor_id, ano);
+    const { titulo, autor_id, ano } = req.body;
+    const resultado = await livroService.criarlivroService(titulo, autor_id, ano);
 
-        res.status(201).json(resultado);
-    } catch (error) {
-        console.log(error.message);
-        res.status(error.status ?? 500).json({
-            error: error.message
-        });
-    }
+    res.status(201).json(resultado);
 };
 
 async function atualizarLivroController(req, res) {
-    try {
-        const id = req.params.id;
-        const { titulo, autor_id, ano } = req.body;
-        const resultado = await livroService.atualizarLivroService(id, titulo, autor_id, ano);
+    const id = req.params.id;
+    const { titulo, autor_id, ano } = req.body;
+    const resultado = await livroService.atualizarLivroService(id, titulo, autor_id, ano);
 
-        res.status(200).json(resultado);
-    } catch (error) {
-        console.log(error.message);
-        res.status(error.status ?? 500).json({
-            error: error.message
-        });
-    }
+    res.status(200).json(resultado);
 };
 
 async function deletarLivroController(req, res) {
-    try {
-        const id = req.params.id;
-        await livroService.deletarLivroService(id);
+    const id = req.params.id;
+    await livroService.deletarLivroService(id);
 
-        res.status(200).send("Livro deletado com sucesso!");
-    } catch (error) {
-        console.log(error.message);
-        res.status(error.status ?? 500).json({
-            error: error.message
-        });
-    }
+    res.status(200).send("Livro deletado com sucesso!");
 };
 
 async function emprestarLivroController(req, res) {
-    try {
-        const id = req.params.id;
-        const resultado = await livroService.emprestarLivroService(id);
+    const id = req.params.id;
+    const resultado = await livroService.emprestarLivroService(id);
 
-        res.status(200).json(resultado);
-    } catch (error) {
-        console.log(error.message);
-        res.status(error.status ?? 500).json({
-            error: error.message
-        });
-    }
+    res.status(200).json(resultado);
 };
 
 module.exports = {
