08f5e991dfbc19afc325e3539a82ad47438b5d80
Lucas Kaoru <lucasnohara51@gmail.com>
2026-09-24 22:17:26 -0300

    feat: adicionando um middleware global

---
+++ src/middleware/logMiddleware.js
@@ -0,0 +1,7 @@
+function logMiddleware(req, res, next) {
+    console.log(req.method + req.url);
+
+    next()
+};
+
+module.exports = logMiddleware;
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const autorRouters = require("./routers/autorRoutes.js");
 const app = express();
 const PORT = 3001;
 const errorHandler = require("./middleware/errorHandler.js");
+const logMiddleware = require("./middleware/logMiddleware.js");
 
 app.use(express.json());
 
@@ -11,10 +12,12 @@ app.get("/", (req, res) => {
     res.send("Olá, Mundo!");
 });
 
+app.use(logMiddleware);
 app.use(livroRouters);
 app.use(autorRouters);
 
 app.use(errorHandler);
+
 
 app.listen(PORT, () => {
     console.log("Rodando!");
